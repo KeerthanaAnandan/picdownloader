83f93d2bcdedbaadda588f491cb43becf97320d4
keerthanaAnandan <keerthanaanandanak@gmail.com>
2021-02-23 20:59:15 +0530

    filesaver update

--- src/Bodyody.js
+++ src/Bodyody.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { IconButton } from "@material-ui/core";
+import { SaveAlt } from "@material-ui/icons";
+
+import FileSaver from "file-saver";
 
 import "./Body.css";
+// download the image in user's Device
+function download(url, fileName) {
+  FileSaver.saveAs(url, fileName);
+}
+//----------------------------------
 
-function Bodyody({ items, setItems }) {
+function Bodyody({ items, img, tags }) {
+  const alt = tags.split(",")[0];
+  const [imageSatus, setImageSatus] = useState(false);
   return (
     <div className="body-image-area">
+      {/* className "d-none" hides the element  */}
+      {/* If the image is not loaded thn add className "d-none" to Image tag */}
       {items.map((single) => (
-        <img className="image" src={single.webformatURL} alt="images" />
+        <div>
+          <img
+            className={`image  ${!imageSatus && "d-none"}`}
+            src={single.webformatURL}
+            alt={alt}
+            onLoad={() => setImageSatus(true)}
+          />
+
+          {/* If the image is loaded then add className "d-none" to Image tag */}
+
+          <IconButton
+            className="imgCard__button"
+            onClick={() => download(img, alt)}
+          >
+            <SaveAlt />
+          </IconButton>
+        </div>
       ))}
     </div>
   );
